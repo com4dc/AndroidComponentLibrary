e7099225e1f7c7c889797db7aa81236fc0585bf8
komuro.hiraku <h.comcom@gmail.com>
2013-12-05 20:18:42 +0900

    SoundButton作成のためのSoundHelperクラスを作成

---
+++ ComponentLibrary/src/jp/classmethod/android/componentlibrary/helper/SoundHelper.java
@@ -0,0 +1,28 @@
+package jp.classmethod.android.componentlibrary.helper;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.media.SoundPool.OnLoadCompleteListener;
+
+public final class SoundHelper {
+	
+	private static SoundPool soundPool;
+
+	/**
+	 * SoundPoolにLoadして、再生する
+	 * @param context
+	 * @param resId
+	 */
+	public static final void playSound(Context context, int resId) {
+		soundPool = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
+		final int soundId = soundPool.load(context, resId, 1);
+		soundPool.setOnLoadCompleteListener(new OnLoadCompleteListener() {
+			
+			@Override
+			public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+				soundPool.play(soundId, 1.0f, 1.0f, 0, 0, 1.0f);
+			}
+		});
+	}
+}
